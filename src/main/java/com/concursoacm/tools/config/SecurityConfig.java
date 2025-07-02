46ace31cf9c02858b480c4380e13df5df3c68125
Draxler37 <wtfluiso@gmail.com>
2025-07-02 04:07:59 -0400

    Agregando CORS del nuevo dominio

--- src/main/java/com/concursoacm/tools/config/SecurityConfig.java
+++ src/main/java/com/concursoacm/tools/config/SecurityConfig.java
@@ -31,7 +31,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         // Agrega los orígenes permitidos
-        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:8080", "http://127.0.0.1:5500"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:8080", "http://127.0.0.1:5500",
+                "https://apiconcursoacm-production.up.railway.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
